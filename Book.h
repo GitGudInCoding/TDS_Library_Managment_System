d4630fd6f790d73c70085f691a15aab2e7bae796
NgSongYi <xxjustawayxx@gmail.com>
2022-12-24 23:16:32 +0800

    commit

--- Book.h
+++ Book.h
@@ -3,59 +3,7 @@
 
 #include <iostream>
 
-using namespace std;#ifndef BOOK_H_INCLUDED
-#define BOOK_H_INCLUDED
-
-#include <iostream>
-
 using namespace std;
-
-
-class Book
-{
-private:
-    struct BookNode
-    {
-        //Declare data type and items in the linked list
-        int bookID,stuID;
-        string bookName,auth,publ;
-        bool stat;
-
-        BookNode *next; //Pointer to the next node
-    };
-
-    BookNode *head;  //Head pointer of linked list
-    BookNode *temp;  //Pointer point to the latest node in linked list
-    BookNode *disp; //Pointer for display, search and getStatus function
-
-
-
-public:
-
-    Book(); //Default constructor
-
-    ~Book(); //Destructor
-
-    string getStatus() const;
-    int getSize();
-    void addBook(string bname1,string bau1,string bpu1);
-    void delBook(int delNo);
-    void editBook(int editNo);
-    void searchBook();
-    void displayBook();
-    void loadBook();
-    void saveBook();
-    void sortBook();
-    void sortID();
-    void borrowBook();
-    void returnBook();
-
-
-    int size;    //Number of book in linked list
-};
-
-#endif // BOOK_H_INCLUDED
-
 
 
 class Book
